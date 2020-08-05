843e6a69df608ba0b60c4fbd198fb167b5036c5b
StringManolo <manuelvarelacaldas@gmail.com>
2020-08-05 20:01:35 +0200

    Separados componentes en vistas.

--- main.js
+++ main.js
@@ -8,17 +8,60 @@
 
 /* Pages: */
 const About = { 
-  template: '<div>About</div>'
+  template: `
+<div>
+<developer-name></developer-name>                                     <developer-description></developer-description>                       <developer-info></developer-info>
+</div>
+`
 }
 
 const Home = { 
   template: `
-<web-header></web-header>
+<i>Home Template Here</i>
 `
 }
 
 const Projects = { 
-  template: '<div>Projects</div>'
+  template: `
+<div>
+<section id="projects">
+  <article id="diariosm">
+  <h3>DIARIOSM</h3>
+  <iframe src="./projects/diariosm/diariosm.html"></iframe>
+  <p class="description">Newspaper</p>
+  </article>
+  <br />
+  <article id="jex">
+  <h3>JEX</h3>
+  <iframe src="./projects/jex/jex.html"></iframe>
+  <p class="description">Hexadecimal Editor.</p>
+  </article>
+  <br />
+  <article id="passwordValidator">
+  <h3>PASSWORDVALIDATOR</h3>
+  <iframe src="./projects/passwordValidator/passwordValidator.html"></iframe>
+  <p class="description">A valid password is the one that conforms to the following rules:<br />
+  - Minimum length is 8.<br />
+  - Maximum length is 32.<br />
+  - Should contain at least one number.<br />
+  - Should contain at least one special character (such as &, +, @, $, #, %, etc.).<br />
+  - Should not contain spaces.</p>
+  </article>
+  <br />
+  <article id="htmlEntities">
+  <h3>HTMLENTITIES</h3>
+  <iframe src="./projects/htmlEntities/htmlEntities.html"></iframe>
+  <p class="description">Encode your source code using rfc html entities to help prevent Cross Site Scripting atacks in your application.</p>
+  </article>
+  <br />
+  <article id="RXSSGEN">
+  <h3>RXSSGEN</h3>
+  <iframe src="./projects/rxssgen/rxssgen.html"></iframe>
+  <p class="description">Modal Assistent to generate a full working custom payload to exploit Reflected Cross Site Scriping via POST http method.</p>
+  </article>
+  </section>
+</div>
+`
 }
 
 // 2. Define some routes
@@ -47,7 +90,7 @@ const app = new Vue({
   router,
   watch: {
     '$route' (to, from) {
-      alert();
+      
     }
   }
 }).$mount("#app");
