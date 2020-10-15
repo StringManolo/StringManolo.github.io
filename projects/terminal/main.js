00ab03240485dcac058c4a2876173963608ea422
StringManolo <manuelvarelacaldas@gmail.com>
2020-10-15 15:29:48 +0200

    Terminal

---
+++ projects/terminal/main.js
@@ -0,0 +1,110 @@
+import ff from "./ff.js";
+
+ff.activateShortcuts();
+ff.defineShortcut("_", alert);
+
+
+setTimeout(loadSystem, 3000);
+
+/* Move input character indicator to the end */
+ael( $("#terminalInput"), "input", () => {
+  $("#terminalInput").value = $("#terminalInput").value.replace(new RegExp("▊", "g"), "") + "▊";
+});
+
+ael( $("#terminalInput"), "keydown", e => {
+  if (e.key === 'Enter' || e.keyCode === 13) {
+    let aux = $("#terminalInput").value;
+    $("#terminalInput").value = "▊";
+
+
+
+    let span = document.createElement("span");
+    span.innerText = "\n$ " + aux.replace(new RegExp("▊", "g"), "");
+
+    $("#terminalFeedback").appendChild(span);
+
+    let span2 = document.createElement("span");
+    span2.innerText = system( aux.replace(new RegExp("▊", "g"), "") );
+
+    $("#terminalFeedback").appendChild(span2);
+    span2.scrollIntoView();
+    
+
+  }
+});
+
+
+
+function system(param) {
+
+  switch(param) {
+    case "clear":
+      $("#terminalFeedback").innerText = "";
+      return "";
+
+    case "pwd":
+      return `\n/example/`;
+
+    case "":
+      return "";
+
+    default:
+      return `\nbash: ${param}: command not found`; 
+  }
+}
+
+function loadSystem() {
+  $("body").style.backgroundColor = "#000";
+  $("main").style.display = "block";
+  $("#systemBootScreen").style.display = "none";
+}
+
+/*
+function generateFileStructure(os) {
+  let fileStruct;
+  if (/linux/gim.test(os)) {
+    fileStruct = { 
+      "/": {
+        "bin": {
+	  ["clear", "pwd"]
+        },
+        "boot": {
+
+        },
+	"dev": {
+
+        },
+	"etc": {
+          "opt": {
+
+	  },
+	  "X11": {
+
+	  },
+	  "sgml": {
+		  
+	  },
+	  "xml": {
+		  
+	  }
+        },
+	"home": {
+
+        },
+	"lib": {
+
+	},
+	"media": {
+
+        }
+      }
+    }
+  }
+  return fileStruct;
+}
+
+let dirs = generateFileStructure("Linux");
+
+_(dirs["/"]);
+
+*/
