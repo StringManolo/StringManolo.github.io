a718e4ecd17f03487013ee6a502b29110e5490ba
StringManolo <manuelvarelacaldas@gmail.com>
2020-10-17 02:04:21 +0200

    Testing customized notification

--- projects/notificationApi/main.js
+++ projects/notificationApi/main.js
@@ -4,7 +4,19 @@ ff.activateShortcuts();
 ff.defineShortcut("_", alert);
 
 
+
+var img = '/to-do-notifications/img/icon-128.png';
+var text = 'HEY! Your task "' + title + '" is now overdue.';
+var notification = new Notification('To do list', { body: text, icon: img });
+
+
 ael($("#noti"), "click", () => {
+  _("Code is making custom notification...");
+  let nofication = new Notification("StringManolo", {
+    body: `Your Actual Date Is ${new.date()}`,
+    icon: './notiIcon.png'
+  });
+
   _("Asking for permission");
   Notification.requestPermission(
   )
