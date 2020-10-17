0bbb872a771a92764ac58ba62b2b961618e69a8c
StringManolo <manuelvarelacaldas@gmail.com>
2020-10-17 12:38:11 +0200

    test

--- projects/notificationApi/serviceworker.js
+++ projects/notificationApi/serviceworker.js
@@ -1,4 +1,9 @@
-swReg.showNotification("StringManolo", {
+
+
+/*navigator.serviceWorker.getRegistration()
+  .then((reg) => reg.showNotification(title, [options]))*/
+
+window.swReg.showNotification("StringManolo", {
   body: `Hello! xD`,
   icon: './notIcon.png'
 });
