c7d0d390c1a2d7030ff708132f01b151eedfc5df
StringManolo <manuelvarelacaldas@gmail.com>
2020-10-17 09:22:30 +0200

    Trying to find the error

--- projects/notificationApi/main.js
+++ projects/notificationApi/main.js
@@ -9,18 +9,18 @@ ael($("#noti"), "click", () => {
   _("Asking for permission");
   Notification.requestPermission(
   )
-  .then(permission => {
+  /*.then(permission => {
     _(`Permission: ${permission}`);
     if(permission) {
-      /*let n = new Notification("StringManolo", {
+      let n = new Notification("StringManolo", {
         body: `Your Date Is ${new.date()}`,
 	icon: './notIcon.png'
-      }*/
+      }
     }
   })
   .catch(error => {
     _(error)
-  })
+  })*/
 
 });
 
