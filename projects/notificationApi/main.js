04332a20105b344b18c7e652b29b6264e59d5575
StringManolo <manuelvarelacaldas@gmail.com>
2020-10-17 10:24:50 +0200

    Testing service worker notification

--- projects/notificationApi/main.js
+++ projects/notificationApi/main.js
@@ -16,7 +16,7 @@ ael($("#noti"), "click", () => {
       if ('serviceWorker' in navigator) {
 	_("New Service worker...");
         navigator.serviceWorker.register('/serviceworker.js', {
-          scope: '/'
+          scope: '/projects/notificationApi/'
         })
         .then(function(reg) {
           window.swReg = reg;
